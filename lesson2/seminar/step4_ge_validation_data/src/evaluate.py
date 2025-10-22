ba58e62094a5fbf4feba368472c07ebeebb15c12
Anastasiaa_5 <borisova04.10@mail.ru>
2025-10-22 20:39:31 +0300

    hw2

--- lesson2/seminar/step4_ge_validation_data/src/evaluate.py
+++ lesson2/seminar/step4_ge_validation_data/src/evaluate.py
@@ -1,11 +1,9 @@
+import pickle
 import pandas as pd
 import yaml
-import os
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-import mlflow
-import json
-import pickle
+
 
 def load_params():
     with open("params.yaml", "r") as f:
@@ -30,22 +28,8 @@ def evaluate_model():
     y_pred = model.predict(X_test)
     accuracy = accuracy_score(y_test, y_pred)
 
-    rows = len(df)
-    metrics = {
-        "accuracy": float(accuracy),
-        "rows": int(rows)
-    }
+    print(f"Accuracy: {accuracy:.4f}")
 
-    os.makedirs("metrics", exist_ok=True)
-    with open("metrics/metrics.json", "w") as f:
-        json.dump(metrics, f, indent=4)
-
-    mlflow.log_metrics(metrics)
-
-    mlflow.log_artifact("metrics/metrics.json")
-
-    print(f"Сохранённые метрики: accuracy={accuracy:.4f}, num_rows={rows}")
 
 if __name__ == "__main__":
-    with mlflow.start_run():
-        evaluate_model()
+    evaluate_model()
